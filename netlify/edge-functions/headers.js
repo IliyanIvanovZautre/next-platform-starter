423c89561343ae6e45c0d07427f8fcc080c6a8ae
IliyanIvanovZautre <69251957+IliyanIvanovZautre@users.noreply.github.com>
2024-06-12 14:29:53 +0300

    Banica

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -1,16 +1,4 @@
 export default async (request, context) => {
-    const response = await context.next();
-
-    // You need this if you are calling this from the browser
-    // to handle CORS preflight stuff
-    if (request.method === "OPTIONS") {
-        return new Response("ok", {
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-            }
-        });
-    }
-
-    response.headers.set("Test", "test");
-    return response;
+    // Set headers here
+    response.headers.set("Test", "banica");
 };
