3c6544b11aeeb833bd3cd827bde251d55a5089df
IliyanIvanovZautre <69251957+IliyanIvanovZautre@users.noreply.github.com>
2024-06-12 14:35:38 +0300

    Banica 2

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -1,4 +1,7 @@
 export default async (request, context) => {
+    const response = await context.next();
+
     // Set headers here
     response.headers.set("Test", "banica");
+    return response;
 };
