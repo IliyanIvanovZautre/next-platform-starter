830ccdbd8716d90a62257d98fcc84b7a1952174b
Iliyan Ivanov <iliyan.ivanov@zautre.com>
2024-08-08 14:13:20 +0300

    Setts security headers

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -1,7 +1,16 @@
 export default async (request, context) => {
     const response = await context.next();
 
-    // Set headers here
-    response.headers.set("Test", "banica");
+    response.headers.set('X-Frame-Options', 'DENY');
+    response.headers.set('Content-Security-Policy', "default-src 'self';");
+    response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+    response.headers.set(
+        'Permissions-Policy',
+        'geolocation=(), microphone=(), camera=(), midi=(), payment=(), fullscreen=(), gyroscope=(), magnetometer=(), accelerometer=(), bluetooth=(), battery=(), usb=(), window-management=(), display-capture=(self), sync-xhr=()'
+    );
+    response.headers.set('Cross-Origin-Embedder-Policy', 'unsafe-none');
+    response.headers.set('Cross-Origin-Opener-Policy', 'unsafe-none');
+    response.headers.set('Cross-Origin-Resource-Policy', 'unsafe-none');
+
     return response;
 };
